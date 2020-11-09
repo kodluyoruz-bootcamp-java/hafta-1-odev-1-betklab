4c96643a86bfa6fb7b0960dc93d3aa58d3d9686d
betklab <62604133+betklab@users.noreply.github.com>
2020-11-09 21:58:19 +0100

    Odev 1

---
+++ hafta_1_odev_1/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        int result = 0;
+        for(int i = 0; i<2000; i++){
+            if( i%(3*5*7*53) == 0){
+                sum = sum + i; //sum +=i;
+                count = count + 1; //count++;
+
+            }
+
+        }
+       result =  sum / count;
+        System.out.println("Sonuç :" +result);
+    }
+}
